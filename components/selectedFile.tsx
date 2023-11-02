3c6be446983e62f6715546979f7a8196ee34f7fc
nyon3 <tomoya@crosstalk.me>
2023-11-02 18:50:08 +0900

    changed text position to icons

--- components/selectedFile.tsx
+++ components/selectedFile.tsx
@@ -5,7 +5,7 @@ import {transcribeAudio} from "@/lib/transcription";
 import TranscriptionDisplay from "@/components/transcriptionDisplay";
 import CopyToClipboard from "./CopyToClip";
 import { AudioPlay } from './AudioPlay';  // Import AudioPlay component
-import { FaTrash, FaMicrophone } from 'react-icons/fa';
+import { FaTrash, FaFileSignature } from 'react-icons/fa';
 
 interface ListItem {
   url: string;
@@ -43,46 +43,46 @@ const SelectedFile: React.FC<SelectedFileProps> = ({ listItems }) => {
 
   return (
     <>
-     {
-        listItems.map((item, index) => {
-          // Determine the icon color for this specific item
-          const iconClassName = selectedFileUrl === item.url ? "text-current h-5 w-5" : "text-gray-400 h-5 w-5";
+    {
+      listItems.map((item, index) => {
+        const iconClassName = selectedFileUrl === item.url ? "text-current h-5 w-5" : "text-gray-400 h-5 w-5";
 
-          return (
-            <li key={index} className="flex items-center space-x-4">
-              <input
-                type="radio"
-                name="fileSelection"
-                value={item.url}
-                checked={selectedFileUrl === item.url}
-                onChange={handleChange}
-                className="mr-4"
-              />
-              <label className="flex-1">{item.name}</label>
-              <div className="flex space-x-2">
-                <button onClick={handleDelete} className="flex items-center space-x-2">
-                  <FaTrash className={iconClassName} />
-                  <span >Delete</span>
-                </button>
-                <button onClick={handleTranscribe} className="flex items-center space-x-2">
-                  <FaMicrophone className={iconClassName} />
-                  <span>Transcribe</span>
-                </button>
-              </div>
-              <AudioPlay audio={item.url} />
-            </li>
-          );
-        })
-      }
-      {/* Create separator */}
-      <hr className="my-4" />
-      
-      <TranscriptionDisplay data={transcription} />
-      <CopyToClipboard text="Text to Copy">
-        {(copy) => <button onClick={copy}>Copy</button>}
-      </CopyToClipboard>
-  
-    </>
+        return (
+          <li key={index} className="flex items-center space-x-4">
+            <input
+              type="radio"
+              name="fileSelection"
+              value={item.url}
+              checked={selectedFileUrl === item.url}
+              onChange={handleChange}
+              className="mr-4"
+            />
+            <label className="flex-1">{item.name}</label>
+            <div className="flex space-x-2">
+              <button onClick={handleDelete} className="flex flex-col items-center justify-center">
+                <FaTrash className={iconClassName} />
+                <span className="text-sm mt-1">Delete</span>
+              </button>
+              <button onClick={handleTranscribe} className="flex flex-col items-center justify-center">
+                <FaFileSignature className={iconClassName} />
+                <span className="text-sm mt-1">Transcribe</span>
+              </button>
+            </div>
+            <AudioPlay audio={item.url} />
+          </li>
+        );
+      })
+    }
+    {/* Create separator */}
+    <hr className="my-4" />
+    
+    <TranscriptionDisplay data={transcription} />
+    <CopyToClipboard text="Text to Copy">
+      {(copy) => <button onClick={copy}>Copy</button>}
+    </CopyToClipboard>
+
+  </>
+
   );
 };
 
