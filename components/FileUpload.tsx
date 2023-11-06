f7278480f920737be8ef903eb987def5b001c5b2
nyon3 <tomoya@crosstalk.me>
2023-11-06 10:51:38 +0900

    delete comments

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -5,36 +5,6 @@ import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { revalidatePath } from "next/cache";
 import { SubmitButton } from "./submitButton";
 
-// Define an interface for expected file structure
-// interface UploadedFile extends File {
-//     type: string;
-//     size: number;
-//   }
-
-// Helper function to check if the user ID is valid
-// const isValidUserId = (userId) => {
-//     return userId != null;
-//   };
-  
-//   // Helper function to check if the file exists in the form data
-//   const fileExists = (file) => {
-//     return file != null;
-//   };
-  
-//   // Helper function to validate the file type
-//   const isValidFileType = (file) => {
-//     // Here you can list all the MIME types you want to allow
-//     const allowedMimeTypes = ['audio/mpeg', 'audio/wav', 'audio/*'];
-//     return allowedMimeTypes.some(type => file.type === type || file.type.startsWith(type));
-//   };
-  
-//   // Helper function to validate the file size (e.g., less than 10MB)
-//   const isValidFileSize = (file:UploadedFile) => {
-//     const maxSize = 10 * 1024 * 1024; // 10MB in bytes
-//     return file.size <= maxSize;
-//   };
-
-    // Upload file to database using Prisma and Next.js server actions
     const addAudioFile = async(data:FormData) => {
         'use server';
         const session = await getServerSession(authOptions);
