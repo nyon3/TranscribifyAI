89c7f1f6f539bfdc66247637ea8d8cbd48590e1b
nyon3 <tomoya@crosstalk.me>
2023-11-10 19:55:50 +0900

    but fix

--- components/testClient.tsx
+++ components/testClient.tsx
@@ -1,33 +1,31 @@
 // create a new client component with the command: onClick={() => alert('test')}    
 'use client'
 
-import { useState } from 'react'
-import { deleteFile, dataProps } from '@/lib/delete'
-
+import { deleteFile } from '@/lib/delete'
+import { dataProps } from '@/lib/types'
 
 type Props = {
-    data: dataProps
+  data: dataProps
 }
 
-export const DeleteTest = ({data} : Props) => {
-    const handleDelete = async () => {
-      const formData = new FormData();
-      formData.append('url', data.url);
-      if(!formData) alert('No file to delete');
-    
-      try {
-        // assuming deleteFile is a fetch call to your API endpoint
-        await deleteFile(formData);
-        alert('File deleted successfully');
-      } catch (error) {
-        alert('Error deleting file');
-      }
-    };
-    
-    return (
-      <button onClick={handleDelete}>
-          Delete test
-      </button>
-    );
+export const DeleteTest = ({ data }: Props) => {
+  const handleDelete = async () => {
+    const formData = new FormData();
+    formData.append('url', data.url);
+    if (!formData) alert('No file to delete');
+
+    try {
+      // assuming deleteFile is a fetch call to your API endpoint
+      await deleteFile(formData as any);
+      alert('File deleted successfully');
+    } catch (error) {
+      alert('Error deleting file');
+    }
   };
-  
+
+  return (
+    <button onClick={handleDelete}>
+      Delete test
+    </button>
+  );
+};
