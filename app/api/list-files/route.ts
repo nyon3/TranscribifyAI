80c71e88eeb1b3fc161f0d17500520e4af1f0217
nyon3 <tomoya@crosstalk.me>
2023-12-20 21:10:35 +0900

    Commented out AWS S3 code in list-files route

--- app/api/list-files/route.ts
+++ app/api/list-files/route.ts
@@ -1,20 +1,20 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { r2 } from '@/lib/awsConfig';
-import { ListBucketsCommand, ListObjectsV2Command } from '@aws-sdk/client-s3';
+// import type { NextApiRequest, NextApiResponse } from 'next';
+// import { r2 } from '@/lib/awsConfig';
+// import { ListBucketsCommand, ListObjectsV2Command } from '@aws-sdk/client-s3';
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  const params = {
-    Bucket: process.env.R2_BUCKET_NAME || '',
-    MaxKeys: 2,
-  };
+// export async function GET(req: NextApiRequest, res: NextApiResponse) {
+//   const params = {
+//     Bucket: process.env.R2_BUCKET_NAME || '',
+//     MaxKeys: 2,
+//   };
 
-  // const command = new ListBucketsCommand(params);
-  const command = new ListObjectsV2Command(params);
+//   // const command = new ListBucketsCommand(params);
+//   const command = new ListObjectsV2Command(params);
 
-  try {
-    const data = await r2.send(command);
-    return new Response(JSON.stringify(data));
-  } catch (err) {
-    return new Response(err)
-  }
-}
+//   try {
+//     const data = await r2.send(command);
+//     return new Response(JSON.stringify(data));
+//   } catch (err) {
+//     return new Response(err)
+//   }
+// }
